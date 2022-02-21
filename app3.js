2a6ab9cfbf6397640c7cb3ea68abc2b0aac55a4a
Magnus Karström <magnus@yesweb.se>
2022-02-21 15:11:26 +0100

    Created tag Generator

--- app3.js
+++ app3.js
@@ -1,41 +1,45 @@
-const tagsEl = document.getElementById('tags');
-const textArea = document.getElementById('textarea');
+const tagsEl = document.getElementById('tags'); //Get all tags as nodearray
+const textArea = document.getElementById('textarea'); //Get inputs from textarea
 
-textArea.focus();
+textArea.focus(); //put focus on textarea for direct input from keyboard at load
 
+//Create eventlistener for every keyup-event
 textArea.addEventListener('keyup', (ev) => {
-  generateTags(ev.target.value);
+  generateTags(ev.target.value); //send the written words into tag generator
 
   if (ev.key === 'Enter') {
+    //check if the enter-button is pressed
     setTimeout(() => {
+      //If enter is pushed then wait 10ms and clear textarea
       ev.target.value = '';
     }, 10);
 
-    randomSelect();
+    randomSelect(); //Run random selection function
   }
 });
 
 function generateTags(input) {
+  //grab input from what is sent from textarea eventlistener
   const tags = input
-    .split(',')
-    .filter((tag) => tag.trim() !== '')
-    .map((tag) => tag.trim());
+    .split(',')//split into array elements when separated with a comma
+    .filter((tag) => tag.trim() !== '')//filter out the tags with whitespaces
+    .map((tag) => tag.trim());//put every trimmed tag back into the tag array
 
-  tagsEl.innerHTML = '';
+  tagsEl.innerHTML = ''; //clear the textarea when it has been sent
 
-  tags.forEach((tag) => {
+  tags.forEach((tag) => {//create tags with new spans in document
     const tagEl = document.createElement('span');
     tagEl.classList.add('tag');
     tagEl.innerText = tag;
-    tagsEl.appendChild(tagEl);
+    tagsEl.appendChild(tagEl);//place the created tags as children to the tags element 
   });
 }
 
-function randomSelect() {
-  const times = 30;
+function randomSelect() { //random selector function
+  const times = 30; //number of times to randomize
 
-  const interval = setInterval(() => {
-    const randomTag = pickRandomTag();
+  const interval = setInterval(() => { //create an interval function that is run for 100ms
+    const randomTag = pickRandomTag();//generate a constant with a random tag as a result
 
     highlightTag(randomTag);
 
--- app4.js
+++ app4.js
@@ -1,5 +1,30 @@
-const tagsEl = document.querySelectorAll('tags');
-const textarea = document.querySelector('textarea');
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
 
 textarea.focus();
 
+textarea.addEventListener('keyup', (ev) => {
+  generateTags(ev.target.value);
+  if (ev.key === 'Enter') {
+    setTimeout(() => {
+      ev.target.value = '';
+    }, 10);
+    randomSelect();
+  }
+});
+
+function generateTags(input) {
+  const tags = input
+    .split(',')
+    .filter((tag) => tag.trim() !== '')
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = '';
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
